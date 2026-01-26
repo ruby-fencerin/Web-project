cbfefa5f2ccb063fbf03d7c92fe5beaa0ad0f3a5
bugybon <bugybon1@gmail.com>
2026-01-26 23:55:41 +0200

    favicon added

--- app/pages/login_page.php
+++ app/pages/login_page.php
@@ -5,6 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel = "stylesheet" href = "../css/login_styles.css">
     <title>Login</title>
+
+    <!-- The images that are needed for all sorts of icons in browsers -->
+    <link rel="icon" type="image/png" href="/images/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/images/favicon.svg" />
+    <link rel="shortcut icon" href="/images/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+    <link rel="manifest" href="/images/site.webmanifest" />
 </head>
 <body>
     <form id = "login-form">
--- app/pages/user_page.php
+++ app/pages/user_page.php
@@ -8,6 +8,13 @@
     <link rel = "stylesheet" href = "../css/menu_styles.css">
     <link rel = "stylesheet" href = "../css/user_page_styles.css">
     
+    <!-- The images that are needed for all sorts of icons in browsers -->
+    <link rel="icon" type="image/png" href="/images/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="/images/favicon.svg" />
+    <link rel="shortcut icon" href="/images/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+    <link rel="manifest" href="/images/site.webmanifest" />
 </head>
 <body>
     <div id = "nav-div">
